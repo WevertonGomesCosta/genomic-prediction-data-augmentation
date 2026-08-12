fbf01c7a2255c16e4ea1b1f7eb89d8d1e7c0d6f3
Weverton Gomes da Costa <95870682+WevertonGomesCosta@users.noreply.github.com>
2026-08-12 15:52:17 -0300

    Declare tutorial dependencies for renv

---
+++ _dependencies.R
@@ -0,0 +1,10 @@
+# Dependências do ambiente R usadas pelo tutorial.
+# Este arquivo é lido pelo renv para detectar também pacotes usados em chunks
+# com eval = FALSE. Ele não faz parte do pipeline analítico.
+
+library(workflowr)
+library(BGLR)
+library(ggplot2)
+library(ggthemes)
+library(knitr)
+library(rmarkdown)
